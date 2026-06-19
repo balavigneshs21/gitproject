e7569804a0eb61118d3ea584bb3aa57a23f4dc5f
balavigneshs21 <balavignesh15112004@gmail.com>
2026-06-19 17:31:09 +0530

    new feature added

--- hello.java
+++ hello.java
@@ -2,8 +2,12 @@ class Main{
     public static void main(String[] args) {
         System.out.println("hello bala");
         test();
+        test2()
     }
     public static void test() {
         System.out.println("test");
+    }
+    public static void test2() {
+        System.out.println("test2");
     }
 }
